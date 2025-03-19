8aed2b50f42de09194257dc5e65829b57daa98b3
Olesia Vysotina <vysotina2002@gmail.com>
2025-03-19 23:08:28 +0100

    first init

---
+++ Bloom_filter.py
@@ -0,0 +1,27 @@
+class BloomFilter:
+    def __init__(self, size, num_hashes):
+        self.size = size
+        self.num_hashes = num_hashes
+        self.bit_array = [0] * self.size
+
+
+def check_password_uniqueness(bloom_filter, passwords):
+    pass
+
+
+if __name__ == "__main__":
+    # Ініціалізація фільтра Блума
+    bloom = BloomFilter(size=1000, num_hashes=3)
+
+    # Додавання існуючих паролів
+    existing_passwords = ["password123", "admin123", "qwerty123"]
+    for password in existing_passwords:
+        bloom.add(password)
+
+    # Перевірка нових паролів
+    new_passwords_to_check = ["password123", "newpassword", "admin123", "guest"]
+    results = check_password_uniqueness(bloom, new_passwords_to_check)
+
+    # Виведення результатів
+    for password, status in results.items():
+        print(f"Пароль '{password}' - {status}.")
---
+++ HyperLogLog_method.py
@@ -0,0 +1,7 @@
+# Результати порівняння:
+#                        Точний підрахунок   HyperLogLog
+# Унікальні елементи              100000.0      99652.0
+# Час виконання (сек.)                0.45          0.1
+
+print("Results of comparison:")
+print(f"{'Exact count':<20}{'HyperLogLog':<20}")
